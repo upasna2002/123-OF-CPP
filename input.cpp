b7f7d3ae9a0921a813bcab85d9c842001f6c5696
upasna2002 <91944232+upasna2002@users.noreply.github.com>
2022-07-12 10:09:51 +0530

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+
+    int n;
+    int r;
+    cin>>n;
+    cin>>r;
+   //  cout<<" user ne enter kia";
+   //  cout<<n;
+   cout<<" user ne enter kia"<< n <<'\n';
+   cout<<"read r"<< r << '\n';
+
+     return 0;
+}
